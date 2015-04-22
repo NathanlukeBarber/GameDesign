eb37c77869b21f7db915e24723ce4fd65b4f49a5
NathanlukeBarber <nathan_luke_barber@hotmail.com>
2015-04-22 11:55:37 +0100

    More models and fixs
    ^

---
+++ New Unity Project 1/Assets/MyAssets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (CharacterController))]
+public class PlayerController : MonoBehaviour {
+	
+	// Handling
+	public float rotationSpeed = 450;
+	public float walkSpeed = 5;
+	public float runSpeed = 8;
+	
+	// System
+	private Quaternion targetRotation;
+	
+	// Components
+	private CharacterController controller;
+	
+	
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"),0,Input.GetAxisRaw("Vertical"));
+		
+		if (input != Vector3.zero) {
+			targetRotation = Quaternion.LookRotation(input);
+			transform.eulerAngles = Vector3.up * Mathf.MoveTowardsAngle(transform.eulerAngles.y,targetRotation.eulerAngles.y,rotationSpeed * Time.deltaTime);
+		}
+		
+		Vector3 motion = input;
+		motion *= (Mathf.Abs(input.x) == 1 && Mathf.Abs(input.z) == 1)?.7f:1;
+		motion *= (Input.GetButton("Run"))?runSpeed:walkSpeed;
+		motion += Vector3.up * -8;
+		
+		controller.Move(motion * Time.deltaTime);
+	}
+}
